4acf0811cb25a7526301961cb55ec88c624f0f1f
soejun <chan.wilson.wc@gmail.com>
2025-10-19 17:41:48 -0400

    wip: unplug-auto-import config

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,12 +1,18 @@
+import AutoImport from 'unplugin-auto-import/vite'
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+
+import Vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    AutoImport({
+      imports: ['vue', 'pinia'],
+      dts: true
+    }),
+    Vue()
   ],
   resolve: {
     alias: {
@@ -15,6 +21,6 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 5173
   }
 })
